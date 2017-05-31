cb6bc483a50b71b0352799d6b63ccbb3ae48259b
yong heng <yongheng@live.com.sg>
2017-05-31 23:38:17 +0800

    test nodata

--- frontend/components/noData.jsx
+++ frontend/components/noData.jsx
@@ -1,7 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 export default function() {
     return (
-<div className="container">
-  <p> No topics available </p>
-</div>
+        <div className="container">
+            <p> No topics available </p>
+        </div>
     )
 }
